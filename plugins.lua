9d9fa185a8cfc1057fce0b12849278b66f0e1b3e
sobitoks <gerardalfonso9@protonmail.com>
2024-02-21 16:34:09 +0100

    Custom folder nvim

---
+++ plugins.lua
@@ -0,0 +1,21 @@
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+return plugins
